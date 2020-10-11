d528281afead88686788f8b3b872891312e57f49
blmvxer <quegmeister@gmail.com>
2020-10-11 20:09:54 +0000

    Created const for google and cloudflare, streamlined code

--- netTest.nimble
+++ netTest.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.2"
+version       = "0.1.3"
 author        = "blmvxer"
 description   = "Connection Test for Nim Web Applications"
 license       = "MIT"
--- src/netTest.nim
+++ src/netTest.nim
@@ -5,36 +5,11 @@ import net, nativesockets
 var
   servers: seq[string]
   socket: Socket = newSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
+const
+  cloudflare* = @["1.1.1.1", "1.0.0.1"]
+  google* = @["8.8.4.4", "8.8.8.8"]
 
-proc testConn(service: string, timeOut: int): seq[bool] =
-  if service == ("google"):
-    servers = @["8.8.4.4", "8.8.8.8"]
-  elif service == ("cloudflare"):
-    servers = @["1.1.1.1", "1.0.0.1"]
-  else:
-    return @[false, false]
-  for server in servers:
-    try:
-      socket.connect(server, Port(53), timeOut)
-      result.add(true)
-    except:
-      result.add(false)
-    finally:
-      socket.close()
-      socket = newSocket()
-  return result
-
-proc testConn*(server: string, port: int, timeOut: int): bool =
-  if isIpAddress(server) == false:
-    return false
-  else:
-    try:
-      socket.connect(server, Port(port), timeOut)
-      return true
-    except:
-      return false
-    finally:
-      socket.close()
+export cloudflare, google
 
 proc testConn*(servers: seq[string], port: int, timeOut: int): seq[bool] =
   for server in servers:
@@ -50,3 +25,15 @@ proc testConn*(servers: seq[string], port: int, timeOut: int): seq[bool] =
         socket.close()
         socket = newSocket()
   return result
+
+proc testConn*(server: string, port: int, timeOut: int): bool =
+  if isIpAddress(server) == false:
+    return false
+  else:
+    try:
+      socket.connect(server, Port(port), timeOut)
+      return true
+    except:
+      return false
+    finally:
+      socket.close()
