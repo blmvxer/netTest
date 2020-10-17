d1af7c8d78549e0c125f69bd6a4d597993f49ba0
blmvxer <quegmeister@gmail.com>
2020-10-16 20:45:55 -0700

    new proc added

--- src/netTest.nim
+++ src/netTest.nim
@@ -1,30 +1,30 @@
-import net, nativesockets
+import net, nativesockets, os
 
 {.hints: off, warnings: off, optimization: speed.}
 
-var
-  servers: seq[string]
-  socket: Socket = newSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
 const
   cloudflare* = @["1.1.1.1", "1.0.0.1"]
   google* = @["8.8.4.4", "8.8.8.8"]
 
 type
   Result = object
-    connects: int
-    disconnects: int
-    conPer: float
-    disPer: float  
+    host: string
+    connects, disconnects: int
+
+var
+  result: Result
+  servers: seq[string]
+  socket: Socket = newSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
 
 export cloudflare, google
 
-proc testConn*(servers: seq[string], port: int, timeOut: int): seq[bool] =
+proc testConn*(servers: seq[string], port: int, timeout: int): seq[bool] =
   for server in servers:
     if isIpAddress(server) == false:
       result.add(false)
     else:
       try:
-        socket.connect(server, Port(port), timeOut)
+        socket.connect(server, Port(port), timeout)
         result.add(true)
       except:
         result.add(false)
@@ -33,15 +33,31 @@ proc testConn*(servers: seq[string], port: int, timeOut: int): seq[bool] =
         socket = newSocket()
   return result
 
-proc testConn*(server: string, port: int, timeOut: int): bool =
+proc testConn*(server: string, port: int, timeout: int): bool =
   if isIpAddress(server) == false:
     return false
   else:
     try:
-      socket.connect(server, Port(port), timeOut)
+      socket.connect(server, Port(port), timeout)
       return true
     except:
       return false
     finally:
       socket.close()
       socket = newSocket()
+
+proc timerConn*(server: string, port: int, timeout: int, timer: int): tuple[host: string, connects: int, disconnects: int] =
+  var
+    x: int
+  x = 0
+  result.host = server
+  while x != timer:
+    if testConn(server, port, timeout) != true:
+      result.disconnects.inc(1)
+      sleep(1000)
+      x.inc(1)
+    else:
+      result.connects.inc(1)
+      sleep(1000)
+      x.inc(1)
+  return result
